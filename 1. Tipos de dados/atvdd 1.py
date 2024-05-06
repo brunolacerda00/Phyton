7d75383dd75ad47cd152642c67517e7a1ab0ae25
Bruno lacerda <161033800+brunolacerda00@users.noreply.github.com>
2024-05-06 14:20:39 +0000

    main

---
+++ 1. Tipos de dados/atvdd 1.py
@@ -0,0 +1,17 @@
+import os
+os.system("cls || clear")
+
+nome = input("Digite seu nome: ")
+idade = int(input("Digite sua idade: "))
+nota1 = int(input("Digite sua primeira nota: "))
+nota2 = int(input("Digite sua segunda nota: "))
+soma = nota1 + nota2
+
+media = soma / 2
+
+print("\n ===RESULTADOS=== \n")
+print(f"Nome: {nome}")
+print(f"Idade: {idade}")
+print(f"Primeira nota: {nota1}")
+print(f"Segunda nota: {nota2}")
+print(f"Média: {media}")
---
+++ 1. Tipos de dados/solicitando dados.py
@@ -0,0 +1,8 @@
+import os
+os.system("cls || clear")
+
+print("Solicitando dados do usuário.")
+nome = input("Digite seu nome: ")
+idade = int(input("Digite sua idade: "))
+peso = float(input("Digite seu peso: "))
+
